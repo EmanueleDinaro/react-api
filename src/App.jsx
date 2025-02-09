027611f7e8f094599575a16b6d4901a3ca9395b4
Emanuele Dinaro <ema.dinaro@gmail.com>
2025-02-09 17:25:27 +0100

    fix code and re-fetch alla BE data

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,39 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
-function App() {
-  const [productList, setProductList] = useState([]);
-  const [newProduct, setNewProduct] = useState("");
+const initialData = {
+  title: "",
+  content: "",
+  image: "",
+  tags: "",
+};
+
+export default function App() {
+  // inizializzo lo stato (con un array vuoto) per la lista degli elementi invocati tramite chiamate alle API
   const [foodsPost, setFoodsPost] = useState([]);
+  // inizializzo lo stato (con un oggetto vuoto) per i dati del form
+  const [formData, setFormData] = useState(initialData);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const newProductData = { title: newProduct };
-
-    axios
-      .post("http://127.0.0.1:3000/foods", newProductData)
-      .then((res) => {
-        setFoodsPost([...foodsPost, res.data]);
-        setNewProduct("");
-      })
-      .catch((error) => {
-        console.error("Errore nell aggiunta del prdotto", error);
-      });
+  // Tramite una funzione paramentrica gestisco il cambiamento dei campi del form tramite il fieldname e il valore
+  const handleFormField = (fieldName, value) => {
+    setFormData((currentFormData) => ({
+      ...currentFormData,
+      [fieldName]: value,
+    }));
   };
 
-  const deleteProduct = (productToDelete) => {
-    setProductList((currentProduct) =>
-      currentProduct.filter((product) => product !== productToDelete)
-    );
+  // Tramite una funzione gestisco la submit del form
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // 1. Aggiungo i dati del form alla lista
+    setList((currentList) => [...currentList, formData]);
+    // 2. Reinizializzo i dati del form con l'oggetto vuoto
+    setFormData({
+      title: "",
+      content: "",
+      image: "",
+      tags: "",
+    });
   };
 
   function fetchFoodsPost() {
@@ -40,28 +48,70 @@ function App() {
   useEffect(fetchFoodsPost, []);
 
   return (
-    <>
-      <h1>Products List</h1>
+    <div>
+      <h1>React form multifield</h1>
       <ul>
+        {/* Mappo la lista degli elementi e renderizzo un elemento <li> per ogni elemento */}
         {foodsPost.map((food) => (
           <li key={food.id}>
-            {food.title}
-            <button onClick={() => deleteProduct(food.title)}>Cancella</button>
+            <strong>Titolo:</strong> {food.title}
+            <br />
+            <strong>Contenuto:</strong> {food.content}
+            <br />
+            <img src={food.image} alt={food.title} />
+            <br />
+            <strong>Tags:</strong> {food.tags}
           </li>
         ))}
       </ul>
-      <hr />
+
       <form onSubmit={handleSubmit}>
+        {/* Input per il titolo */}
         <input
           type="text"
-          value={newProduct}
-          onChange={(event) => setNewProduct(event.target.value)}
-          placeholder="Inserisci un nuovo prodotto"
+          placeholder="Inserisci l'autore"
+          name="title"
+          value={formData.title}
+          onChange={(event) => handleFormField("title", event.target.value)}
+          required
         />
-        <button type="submit">Aggiungi</button>
+        <br />
+        {/* Input per il contenuto */}
+        <input
+          type="text"
+          placeholder="Inserisci il contenuto"
+          name="content"
+          value={formData.content}
+          onChange={(event) => handleFormField("content", event.target.value)}
+          required
+        />
+        <br />
+        {/* Selezione per la categoria */}
+        <select
+          name="category"
+          value={formData.category}
+          onChange={(event) => handleFormField("category", event.target.value)}
+          required
+        >
+          <option value="" hidden>
+            Seleziona la categoria
+          </option>
+          <option value="frontend">FrontEnd</option>
+          <option value="backend">BackEnd</option>
+          <option value="uiux">UI/UX</option>
+        </select>
+        <br />
+        {/* Checkbox per la pubblicazione */}
+        <input
+          name="isPublished"
+          type="checkbox"
+          checked={formData.isPublished}
+          onChange={(event) =>
+            handleFormField("isPublished", event.target.checked)
+          }
+        />
+        <button type="submit">Invia</button>
       </form>
-    </>
+    </div>
   );
 }
-
-export default App;
